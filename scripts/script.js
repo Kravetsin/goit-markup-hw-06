d7860ac95def04237427fe59947985a1f42b7172
Kravets <106190014+Kravetsin@users.noreply.github.com>
2025-06-14 23:51:26 +0200

    modal window

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// !MENU
 const menu = document.querySelector(".btn-menu");
 const closeMenu = document.querySelector(".btn-close");
 const nav = document.querySelector(".box-nav");
@@ -8,4 +9,17 @@ menu.addEventListener("click", () => {
 
 closeMenu.addEventListener("click", () => {
   nav.classList.remove("is-open");
+});
+
+// !MODAL WINDOW
+const orderButton = document.querySelector(".btn-order");
+const modalWindow = document.querySelector(".box-modal-bg");
+const closeModal = document.querySelector(".btn-modal");
+
+orderButton.addEventListener("click", () => {
+  modalWindow.classList.add("is-open");
+});
+
+closeModal.addEventListener("click", () => {
+  modalWindow.classList.remove("is-open");
 });
